9f13a1a55f5f8a64ee630c9bf251917b4e56a0f9
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2023-12-08 16:31:14 +0200

    WIP

--- main3.scm
+++ main3.scm
@@ -5,6 +5,8 @@
 (use-modules (sportsim2))
 (use-modules (db sqlite3))
 
+(define db-schema-version 1)
+
 (define conf-n-countries 30)
 (define conf-n-teams-per-country 30)
 (define conf-n-teams-per-division 6)
@@ -12,36 +14,43 @@
 (define conf-n-players-in-match 2)
 (define conf-n-teams-promoted 3)
 
-(define-record-type <status>
-  (make-status key value)
-  status?
-  (id status-id)
-  (key status-key)
-  (value status-value))
+(define-record-type <keyval>
+  (make-keyval id key value)
+  keyval?
+  (id keyval-id)
+  (key keyval-key)
+  (value keyval-value))
 
 (define-record-type <country>
-  (make-country name)
+  (make-country id name)
   country?
   (id country-id)
   (name country-name))
 
 (define-record-type <team>
-  (make-team name)
+  (make-team id name)
   team?
   (id team-id)
   (name team-name)
   (country_id team-country))
 
 (define all-types
-  (list <status> <country> <team>))
+  (list <keyval> <country> <team>))
 
-(define (sql-insert-col-list rtd)
-  (string-append
-    "("
-    (string-join
-      (map symbol->string (record-type-fields rtd))
-      ", ")
-    ")"))
+(define load-keyval-value
+  (case-lambda
+    ((db key)
+     (load-keyval-value db key #f))
+    ((db key default)
+      (define records
+        (sqlite3-execute-select
+          db
+          (rtd-to-table-name <keyval>)
+          '(id key value)
+          make-keyval
+          (list (cons 'key key))
+          1))
+      (if (null? records) default (keyval-value (car records))))))
 
 (define (sql-create-table-col-list rtd)
   (string-append
@@ -56,84 +65,48 @@
       ", ")
     ")"))
 
-(define (sql-insert-placeholders-list rtd)
-  (string-append
-    "("
-    (string-join
-      (map (lambda (_) "?") (record-type-fields rtd))
-      ", ")
-    ")"))
-
-(define (pars-list record)
-  (let ((rtd (record-type-descriptor record)))
-    (map
-      (lambda (field)
-        ((record-accessor rtd field) record))
-      (record-type-fields rtd))))
-
 (define (rtd-to-table-name rtd)
   (assoc-ref
     (list
-      (cons <status> "sim_status")
-      (cons <country> "sim_country")
-      (cons <team> "sim_team"))
+      (cons <keyval> 'sim_keyval)
+      (cons <country> 'sim_country)
+      (cons <team> 'sim_team))
     rtd))
-
-(define (save-record db record)
-  (define rtd (record-type-descriptor record))
-  (define sql
-    (string-replace-substring
-      (string-replace-substring
-        (string-replace-substring
-          "insert or replace
-          into {table}
-          {col-list}
-          values
-          {placeholders-list}"
-          "{table}" (rtd-to-table-name rtd))
-        "{col-list}" (sql-insert-col-list rtd))
-      "{placeholders-list}" (sql-insert-placeholders-list rtd)))
-  (define pars (pars-list record))
-  (sqlite3-execute db sql pars))
 
 (define (gen-countries db)
   (for-each
     (lambda (_)
-      (let ((record (make-country (string-append "country-" (uuid)))))
-        (save-record db record)))
+      (let ((record (make-country #f (string-append "country-" (uuid)))))
+        (sqlite3-save-record db record)))
     (range 0 conf-n-countries)))
 
 (define (create-tables db)
   (for-each
     (lambda (rtd)
-      (sqlite3-execute db
+      (sqlite3-execute-sql db
         (string-replace-substring
           (string-replace-substring
-            "create table if not exists
-            {table}
-            {col-list}"
+            (string-append
+              "create table if not exists "
+              "{table} "
+              "{col-list}")
             "{table}" (rtd-to-table-name rtd))
           "{col-list}" (sql-create-table-col-list rtd))))
-    all-types))
+    all-types)
+  (sqlite3-save-record
+    db rtd-to-table-name
+    (make-keyval #f "db-schema-version" db-schema-version)))
 
 (define (generate-entities db)
-  (when (not (null?
-      (sqlite3-execute db
-        "select 1
-        from sim_status
-        where key = 'generate-entities-done'
-        and value = 1
-        limit 1")))
+  (when (not (equal? (load-keyval-value db "generate-entities-done") 1))
     (gen-countries db)
-    (save-record db (make-status "generate-entities-done" 1))))
+    (sqlite3-save-record db rtd-to-table-name
+     (make-keyval #f "generate-entities-done" 1))))
 
 (define (main)
   (define db (sqlite3-open "db.db"))
   (create-tables db)
   (generate-entities db)
-
-  (gen-countries db)
-
   (sqlite3-close db)
 )
 
