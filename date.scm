6bf5a514f912459615eb00c0b91bd63dbff79d25
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2023-12-12 09:36:29 +0200

    WIP

---
+++ date.scm
@@ -0,0 +1,293 @@
+(define-module (date))
+
+(use-modules (srfi srfi-19))
+
+(export
+  date
+  date=?
+  date<?
+  date<=?
+  date>?
+  date>=?
+  ts->date
+  date->ts
+  iso-8601-date
+  add-day
+  add-days
+  add-months
+  add-years
+  next-date-for-schedule
+)
+
+(define (lpad s l c)
+  (let loop ((s s))
+    (if (< (string-length s) l)
+      (loop (string-append c s))
+      s)))
+
+(define (iso-8601-date date)
+  (string-append
+    (lpad (number->string (date-year date)) 4 "0")
+    "-"
+    (lpad (number->string (date-month date)) 2 "0")
+    "-"
+    (lpad (number->string (date-day date)) 2 "0")))
+
+(define (div-irregular a bs)
+  ; Returns (r1 . r2)
+  ; where a = (sum bs[:r1]) + r2
+  (if (equal? bs '())
+    (list 0 a)
+    (let ((b (car bs)))
+      (if (< a b)
+        (list 0 a)
+        (let* (
+          (p (div-irregular (- a b) (cdr bs)))
+          (r1 (list-ref p 0))
+          (r2 (list-ref p 1)))
+            (list (+ 1 r1) r2))))))
+
+; TS2000 : Timestamp at 2000-01-01
+(define TS2000  (* (+ 7 (* 30 365)) 24 60 60))
+(define secs-in-day (* 24 60 60))
+(define days-in-std-4-years (+ 1 (* 4 365)))
+(define days-in-400-years (- (* 100 days-in-std-4-years) 3))
+(define secs-in-400-years (* days-in-400-years secs-in-day))
+(define secs-in-non-leap-year (* secs-in-day 365))
+(define secs-in-leap-year (* secs-in-day 366))
+(define secs-in-std-4-years (* secs-in-day days-in-std-4-years))
+(define secs-in-non-std-4-years (* secs-in-day 365 4))
+
+(define (ts->date secs-since-1970)
+  (let* (
+      (start-year 2000)
+      (secs-since-start-year (- secs-since-1970 TS2000))
+      (q400 (floor-quotient secs-since-start-year secs-in-400-years))
+      (r400 (floor-remainder secs-since-start-year secs-in-400-years))
+      (p100 (div-irregular r400
+        (cons
+          (+ secs-in-std-4-years (* 24 secs-in-std-4-years))
+          (make-list
+            3 (+ secs-in-non-std-4-years (* 24 secs-in-std-4-years))))))
+      (q100 (list-ref p100 0))
+      (r100 (list-ref p100 1))
+      (p4 (div-irregular r100
+        (cons
+          (if (= q100 0) secs-in-std-4-years secs-in-non-std-4-years)
+          (make-list 24 secs-in-std-4-years))))
+      (q4 (list-ref p4 0))
+      (r4 (list-ref p4 1))
+      (is-non-std? (and (= q4 0) (or (= q100 1) (= q100 2) (= q100 3))))
+      (p1 (div-irregular r4
+        (cons
+          (if is-non-std? secs-in-non-leap-year secs-in-leap-year)
+          (make-list 3 secs-in-non-leap-year))))
+      (q1 (list-ref p1 0))
+      (r1 (list-ref p1 1))
+      (leap? (and (not is-non-std?) (= q1 0)))
+      (days-per-month (list 31 (if leap? 29 28) 31 30 31 30 31 31 30 31 30 31))
+      (p-month (div-irregular r1
+        (map (lambda (d) (* d secs-in-day)) days-per-month)))
+      (q-month (list-ref p-month 0))
+      (r-month (list-ref p-month 1))
+      (q-days (quotient r-month secs-in-day))
+      (r-days (remainder r-month secs-in-day))
+      (q-hours (quotient r-days 3600))
+      (r-hours (remainder r-days 3600))
+      (q-minutes (quotient r-hours 60))
+      (r-minutes (remainder r-hours 60))
+      (q-seconds r-minutes)
+      (year (+ start-year (* 400 q400) (* 100 q100) (* 4 q4) q1)))
+    (make-date
+      0 q-seconds q-minutes q-hours (+ 1 q-days) (+ 1 q-month) year 0)))
+
+(define (date->ts date)
+  (let* (
+      (start-year 2000)
+      (years-since-start-year (- (date-year date) start-year))
+      (q400 (floor-quotient years-since-start-year 400))
+      (r400 (floor-remainder years-since-start-year 400))
+      (q100 (quotient r400 100))
+      (r100 (remainder r400 100))
+      (q4 (quotient r100 4))
+      (r4 (remainder r100 4))
+      (q1 r4)
+      (non-leap? (or (> q1 0) (and (= q4 0) (> q100 0))))
+      (month (date-month date))
+      (days-per-month
+        (list 31 (if non-leap? 28 29) 31 30 31 30 31 31 30 31 30 31))
+      (day (date-day date))
+      (hour (date-hour date))
+      (minute (date-minute date))
+      (second (date-second date)))
+    (+
+      TS2000
+      (* q400 secs-in-400-years)
+      (cond
+        ((= q100 0) 0)
+        (else (+ (*
+            (- q100 1)
+            (+ secs-in-non-std-4-years (* 24 secs-in-std-4-years))
+          ) (* 25 secs-in-std-4-years))))
+      (cond
+        ((= q4 0) 0)
+        (else (+ (*
+            (- q4 1) secs-in-std-4-years)
+          (if (= q100 0) secs-in-std-4-years secs-in-non-std-4-years))))
+      (cond
+        ((= q1 0) 0)
+        (else (+
+          (* (- q1 1) secs-in-non-leap-year)
+          (if (and (= q4 0) (> q100 0))
+            secs-in-non-leap-year
+            secs-in-leap-year))))
+      (* secs-in-day
+        (let calc-month ((m month) (dpm days-per-month))
+          (cond
+            ((= m 1) 0)
+            (else (+ (car dpm) (calc-month (- m 1) (cdr dpm)))))))
+      (* secs-in-day (- day 1))
+      (* 3600 hour)
+      (* 60 minute)
+      second)))
+
+(define (ts->dow ts)
+  (define r1 (floor-quotient ts secs-in-day))
+  (define dow (remainder (+ r1 3) 7))
+  dow)
+
+(define (date->dow date)
+  (ts->dow (date->ts date)))
+
+(define (leap? year)
+  (cond
+    ((= (remainder year 400) 0) #t)
+    ((= (remainder year 100) 0) #f)
+    ((= (remainder year 4) 0) #t)
+    (else #f)))
+
+(define (add-days date days)
+  (ts->date (+ (* days 3600 24) (date->ts date))))
+
+(define (add-day date)
+  (add-days date 1))
+
+(define (valid-date? date-in)
+  (define year (date-year date-in))
+  (define month (date-month date-in))
+  (define day (date-day date-in))
+  (and
+    (not (= year 0))
+    (>= month 1)
+    (<= month 12)
+    (>= day 1)
+    (<=
+      day
+      (cond
+        ((= month 1) 31)
+        ((= month 2) (if (leap? year) 29 28))
+        ((= month 3) 31)
+        ((= month 4) 30)
+        ((= month 5) 31)
+        ((= month 6) 30)
+        ((= month 7) 31)
+        ((= month 8) 31)
+        ((= month 9) 30)
+        ((= month 10) 31)
+        ((= month 11) 30)
+        ((= month 12) 31)))))
+
+(define (add-months date-in months)
+  (define year (date-year date-in))
+  (define month (date-month date-in))
+  (define day (date-day date-in))
+  (define-values (q r) (floor/ (+ months (1- month)) 12))
+  (define new-month (1+ r))
+  (define new-year (+ year q))
+  (define candidate (date new-year new-month day))
+  (if (valid-date? candidate) candidate #f))
+
+(define (add-years date-in years)
+  (define year (date-year date-in))
+  (define month (date-month date-in))
+  (define day (date-day date-in))
+  (define straddle-zero-year? (and (< year 0) (>= (+ year years) 0)))
+  (define new-year (+ year (+ years (if straddle-zero-year? 1 0))))
+  (define candidate (date new-year month day))
+  (define result (if (valid-date? candidate) candidate #f))
+  result)
+
+(define (date y m d)
+  (make-date 0 0 0 0 d m y 0))
+
+(define compare-dates-procs (list
+  date-year
+  date-month
+  date-day
+  date-hour
+  date-minute
+  date-second
+  date-nanosecond))
+
+(define (compare-dates d1 d2)
+  (let loop ((procs compare-dates-procs))
+    (if (null? procs)
+      0
+      (let* (
+          (proc (car procs))
+          (v1 (proc d1))
+          (v2 (proc d2))
+          (diff (- v1 v2)))
+        (if (= diff 0)
+          (loop (cdr procs))
+          diff)))))
+
+(define (date=? d1 d2)
+  (= (compare-dates d1 d2) 0))
+
+(define (date<? d1 d2)
+  (< (compare-dates d1 d2) 0))
+
+(define (date<=? d1 d2)
+  (<= (compare-dates d1 d2) 0))
+
+(define (date>? d1 d2)
+  (> (compare-dates d1 d2) 0))
+
+(define (date>=? d1 d2)
+  (>= (compare-dates d1 d2) 0))
+
+(define (date-matches? date-in year month day)
+  (and
+    (or (null? year) (= (date-year date-in) year))
+    (or (null? month) (= (date-month date-in) month))
+    (or (null? day) (= (date-day date-in) day))))
+
+(define (max-date-that-matches year month day)
+  (cond
+    ((null? year) #f)
+    ((and (not (null? month)) (not (null? day))) (date year month day))
+    ((and (not (null? month)) (null? day))
+      (add-days (add-months (date year month 1) 1) -1))
+    ((and (null? month) (not (null? day)))
+      (date year 12 day))
+    ((and (null? month) (null? day))
+      (date year 12 31))))
+
+(define
+    (next-date-for-schedule as-of-date
+      schedule-year schedule-month schedule-day)
+  (define max-date-for-schedule
+    (max-date-that-matches schedule-year schedule-month schedule-day))
+  (define result
+    (let loop ((as-of-date as-of-date))
+      (if (and
+            (date? max-date-for-schedule)
+            (date>? as-of-date max-date-for-schedule))
+        #f
+        (let ()
+          (if (date-matches?
+                as-of-date schedule-year schedule-month schedule-day)
+            as-of-date
+            (loop (add-day as-of-date)))))))
+  result)
--- main3.scm
+++ main3.scm
@@ -1,5 +1,6 @@
 (use-modules (srfi srfi-9))
 (use-modules (ice-9 string-fun))
+(use-modules (date))
 (use-modules (util))
 (use-modules (math))
 (use-modules (sportsim2))
@@ -7,12 +8,14 @@
 
 (define db-schema-version 1)
 
-(define conf-n-countries 30)
-(define conf-n-teams-per-country 30)
+(define conf-n-countries 3)
+(define conf-n-teams-per-country 18)
 (define conf-n-teams-per-division 6)
 (define conf-n-players-per-team 4)
 (define conf-n-players-in-match 2)
 (define conf-n-teams-promoted 3)
+(define conf-sim-start-date (date 2023 8 1))
+(define conf-sim-end-date (date 2025 7 1))
 
 (define-record-type <keyval>
   (make-keyval id key value)
@@ -32,25 +35,43 @@
   team?
   (id team-id)
   (name team-name)
-  (country_id team-country))
+  (country_id team-country_id))
+
+(define-record-type <competition>
+  (make-competition id name country_id season)
+  competition?
+  (id competition-id)
+  (name competition-name)
+  (country_id competition-country_id)
+  (season competition-season))
 
 (define all-types
-  (list <keyval> <country> <team>))
+  (list <keyval> <country> <team> <competition>))
+
+(define (rtd-to-table-name rtd)
+  (assoc-ref
+    (list
+      (cons <keyval> 'sim_keyval)
+      (cons <country> 'sim_country)
+      (cons <team> 'sim_team)
+      (cons <competition> 'sim_competition))
+    rtd))
 
 (define load-keyval-value
   (case-lambda
     ((db key)
      (load-keyval-value db key #f))
     ((db key default)
-      (define row #f)
+      (define record #f)
       (sqlite3-for-each-by-select
-        (lambda (r) (set! row r))
+        (lambda (r) (set! record r))
+        make-keyval
         db
         (rtd-to-table-name <keyval>)
         (record-type-fields <keyval>)
         (list (cons 'key key))
         1)
-      (if (equal? #f row) default (keyval-value (apply make-keyval row))))))
+      (if (equal? #f record) default (keyval-value record)))))
 
 (define (sql-create-table-col-list rtd)
   (string-append
@@ -65,14 +86,6 @@
       ", ")
     ")"))
 
-(define (rtd-to-table-name rtd)
-  (assoc-ref
-    (list
-      (cons <keyval> 'sim_keyval)
-      (cons <country> 'sim_country)
-      (cons <team> 'sim_team))
-    rtd))
-
 (define (gen-countries db)
   (d "Generating countries...")
   (for-each
@@ -82,14 +95,22 @@
     (range 0 conf-n-countries))
   (d "Done generating countries"))
 
+(define (generate-teams-for-country db country)
+  (for-each
+    (lambda (_)
+      (define record
+        (make-team #f (string-append "team-" (uuid))))
+      (sqlite3-save-record db rtd-to-table-name record))
+    (range 0 conf-n-teams-per-country)))
+
 (define (gen-teams db)
   (d "Generating teams...")
-  (sqlite3-for-each-by-sql
-    (lambda (row)
-      (d 'foo row))
+  (sqlite3-for-each-by-select
+    (lambda (country) (generate-teams-for-country db country))
+    make-country
     db
-    "select * from sim_country"
-    '())
+    (rtd-to-table-name <country>)
+    (record-type-fields <country>))
   (d "Done generating teams"))
 
 (define (create-tables db)
@@ -116,10 +137,19 @@
     (sqlite3-save-record db rtd-to-table-name
      (make-keyval #f "generate-entities-done" 1))))
 
+(define (do-day date)
+  (d (iso-8601-date date)))
+
 (define (main)
   (define db (sqlite3-open "db.db"))
   (create-tables db)
   (generate-entities db)
+
+  (let loop ((date conf-sim-start-date))
+    (when (date<? date conf-sim-end-date)
+      (do-day date)
+      (loop (add-day date))))
+
   (sqlite3-close db)
 )
 
