a6234a28a688580ae07753873f7fef78c914f47f
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2024-01-22 15:56:15 +0200

    WIP

--- main5.scm
+++ main5.scm
@@ -10,6 +10,7 @@
 
 (define conf-n-countries 3)
 (define conf-n-teams-per-country 40)
+(define conf-n-players-per-team 22)
 (define conf-n-players-per-country 2000)
 (define conf-n-managers-per-country 200)
 
@@ -104,32 +105,38 @@
             string-append
             (map symbol->string symbols))))
 
-(define (run-query db tables-in filter-proc)
-    (define tables (if (not (list? tables-in)) (list tables-in) tables-in))
-    (define recs
-        (let loop ((tables tables) (first #t) (results '()))
-            (if (null? tables)
-                results
-                (if first
-                    (loop
-                        (cdr tables)
-                        #f
-                        (let ((table (car tables)))
-                            (hash-ref db table)))
-                    (loop
-                        (cdr tables)
-                        #f
-                        (apply
-                            append
-                            (map
-                                (lambda (rec)
-                                    (let (
-                                            (attr
-                                                (symbol-append
-                                                    (car tables) '-set)))
-                                        (slot-ref rec attr)))
-                                results)))))))
-    (filter filter-proc recs))
+(define run-query
+    (case-lambda
+        ((db tables-in)
+            (run-query db tables-in (lambda (rec) #t)))
+        ((db tables-in filter-proc)
+            (define tables
+                (if (not (list? tables-in)) (list tables-in) tables-in))
+            (define recs
+                (let loop ((tables tables) (first #t) (results '()))
+                    (if (null? tables)
+                        results
+                        (if first
+                            (loop
+                                (cdr tables)
+                                #f
+                                (let ((table (car tables)))
+                                    (hash-ref db table)))
+                            (loop
+                                (cdr tables)
+                                #f
+                                (apply
+                                    append
+                                    (map
+                                        (lambda (rec)
+                                            (let (
+                                                    (attr
+                                                        (symbol-append
+                                                            (car tables)
+                                                            '-set)))
+                                                (slot-ref rec attr)))
+                                        results)))))))
+            (filter filter-proc recs))))
 
 (define combined-filter-proc
     (lambda filter-procs
@@ -162,15 +169,32 @@
                 (filter-proc-single
                     filter-proc-single-proc filter-proc-single-arg ...) ...))))
 
+(define (initial-assign-player-to-team db player team)
+    (db-insert-rec db
+        (make <player-contract>
+            #:id (get-id)
+            #:player player
+            #:team team
+            #:date-start conf-sim-start-date
+            #:date-end
+                (add-days conf-sim-start-date (* 5 365))
+            #:wage 1000.0
+            #:status 'signed)))
 
-(define (init-contract db)
+(define (init-contract db country)
     (define teams
-        (run-query
-            db
-            '<team>
-            (filter-proc
-                (equal? 1 1))))
-    (d teams))
+        (run-query db '<team>
+            (lambda (t) (equal? (slot-ref t 'country) country))))
+    (define players
+        (run-query db '<player>
+            (lambda (p) (equal? (slot-ref p 'country) country))))
+    (let loop ((teams teams) (players players))
+        (unless (null? teams)
+            (for-each
+                (lambda (player)
+                    (initial-assign-player-to-team db player (car teams)))
+                (take players conf-n-players-per-team))
+            (loop (cdr teams) (drop players conf-n-players-per-team)))))
 
 (define (init-sim db)
     (for-each
@@ -179,9 +203,9 @@
                 (db-insert-rec db country)
                 (init-team db country)
                 (init-player db country)
-                (init-manager db country)))
-        (range conf-n-countries))
-    (init-contract db))
+                (init-manager db country)
+                (init-contract db country)))
+        (range conf-n-countries)))
 
 (define (main)
     (define db (make-db))
